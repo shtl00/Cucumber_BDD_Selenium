33dd8a0449c0dd03a373e9977c3c87f0c2b0f70e
shtl00 <parmarsheetal@ymail.com>
2023-12-25 12:52:05 +0000

    CI/CD basket feature

--- src/test/java/org/example/step_definition/BasketSteps.java
+++ src/test/java/org/example/step_definition/BasketSteps.java
@@ -7,13 +7,12 @@ import org.example.pages.BasketPage;
 
 public class BasketSteps extends DriverManager {
     DriverManager driverManager = new DriverManager();
-    BasketPage basketPage=new BasketPage();
+    BasketPage basketPage = new BasketPage();
+
     @Given("^I am on home page$")
     public void i_am_on_home_page() throws Throwable {
 
-
     }
-
     @Given("^I  search product \"([^\"]*)\"$")
     public void i_search_product(String arg1) throws Throwable {
         basketPage.searchProduct();
@@ -23,16 +22,12 @@ public class BasketSteps extends DriverManager {
     @Given("^I click on \"([^\"]*)\" button$")
     public void i_click_on_button(String arg1) throws Throwable {
         basketPage.clickOnAddProduct();
-
-
-
     }
 
     @Then("^I should see \"([^\"]*)\"$")
     public void i_should_see(String checkOuts) throws Throwable {
         basketPage.clickOnBasket();
         basketPage.checkoutVisible();
-        Thread.sleep(1000);
 
     }
 }
